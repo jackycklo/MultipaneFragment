8322ef56fc7194817b4a91ea1d03ea9356af0e1c
jackycklo <jackycklo2@gmail.com>
2017-05-09 19:23:01 +0800

    init commit

---
+++ app/src/main/java/com/jackylo/multipanefragment/MainActivity.java
@@ -0,0 +1,28 @@
+package com.jackylo.multipanefragment;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+
+        int screenOrientation = getResources().getConfiguration().orientation;
+        if (screenOrientation == Configuration.ORIENTATION_PORTRAIT) {
+            hideSidePanel();
+        }
+    }
+
+    private void hideSidePanel() {
+        View sidePane = findViewById(R.id.side_panel);
+        if (sidePane.getVisibility() == View.VISIBLE) {
+            sidePane.setVisibility(View.GONE);
+        }
+
+    }
+}
---
+++ app/src/main/java/com/jackylo/multipanefragment/MainPanelFragment.java
@@ -0,0 +1,27 @@
+package com.jackylo.multipanefragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jackylo on 9/5/2017.
+ */
+
+public class MainPanelFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main_panel, container, false);
+
+        return view;
+    }
+
+
+
+}
